fe99636af7c98a8e6c6c9b33fa979e7f7681b730
Patrick Reynolds <cpreynolds@gmail.com>
2012-08-15 20:43:15 -0400

    Fixing a nasty bug in the auth decorator.
    This resulted in the session getting stuck in a non-rollback
    state on the server.

--- chkphrase/auth.py
+++ chkphrase/auth.py
@@ -12,13 +12,18 @@ import chkphrase.database as db
 def check_auth(username, password):
     """This function is called to check if a username/password combination is
     valid."""
-    result = db.db_session.query(User).filter(User.name==username)
+    session = db.db_session()
+    result = session.query(User).filter(User.name == username)
     if result.count() != 1:
+        session.rollback()
+        session.close()
         return False
     else:
         truePass = result[0].password
         tryPass = hashlib.sha256(password).hexdigest()
+        session.close()
         return truePass == tryPass
+
 
 def authenticate():
     """Sends a 401 response that enables basic auth"""
@@ -26,6 +31,7 @@ def authenticate():
     'Could not verify your access level for that URL.\n'
     'You have to login with proper credentials', 401,
     {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
 
 def requires_auth(f):
     @wraps(f)
