cd9df8d8173d5eae29d7522765c921e3314d83a2
Patrick Reynolds <cpreynolds@gmail.com>
2012-08-15 22:29:47 -0400

    Style and PEP 8 Fixes.

--- chkphrase/__init__.py
+++ chkphrase/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 class WebFactionMiddleware(object):
     def __init__(self, app):
         self.app = app
+
     def __call__(self, environ, start_response):
         environ['SCRIPT_NAME'] = '/chkphrase'
         return self.app(environ, start_response)
